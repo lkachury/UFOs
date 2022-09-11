68cf52bf96885e6159a9f76c2beedaae75fadb51
lizeth achury <lkcachury@hotmail.com>
2022-09-11 17:08:09 -0400

    Upload files

--- js/app.js
+++ js/app.js
@@ -21,3 +21,25 @@ function buildTable(data) {
         });
     });
 }
+
+// Create a function to handle what to do after an input is given
+function handleClick() {
+    // Create a couple of variables to hold the date data
+    let date = d3.select("#datetime").property("value");
+    let filteredData = tableData;
+
+    // Use the "If" Statement to check if a date was entered and filter the data using that date
+    if (date) {
+        // Apply `filter` to the table data to only keep the rows where the `datetime` value matches the filter value
+        filteredData = filteredData.filter(row => row.datetime === date);
+    };
+
+    // Build the Filtered Table 
+    buildTable(filteredData);
+}
+
+// Attach an event to listen for the form button
+d3.selectAll("#filter-btn").on("click", handleClick);
+
+// Build the final table when the page loads
+buildTable(tableData);
